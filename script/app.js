d14479d768633c2e8c101cc709d9d95843ff932e
Youssef Magdy <yma.youssef.21@gmail.com>
2022-10-01 23:52:48 +0200

    added the js code

---
+++ script/app.js
@@ -0,0 +1,34 @@
+const baseURL = "https://api.adviceslip.com/advice";
+
+const getAdviceData = async () => {
+  const response = await fetch(baseURL);
+  console.log(response);
+  try {
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (err) {
+    console.log("something went wrong");
+    console.error(err);
+  }
+};
+
+async function generateAdvice(e) {
+  try {
+    const adviceData = await getAdviceData();
+    adviceID = adviceData.slip.id;
+    adviceText = adviceData.slip.advice;
+    document.getElementById("advice-id").innerHTML = adviceID;
+    document.getElementById("advice-text").innerHTML = adviceText;
+    // console.log("Data displayed successfully");
+  } catch (err) {
+    document.getElementById("advice-text").innerHTML =
+      "Something went wrong .. Please try again.";
+    console.log("something went wrong");
+    console.error(err);
+  }
+}
+
+document
+  .getElementById("generate-advice")
+  .addEventListener("click", generateAdvice);
